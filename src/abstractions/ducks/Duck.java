974899a879936a1055f3e83ce751a4fbb367749f
Ivan Kulakov <shunzhi@Ivans-MacBook-Pro.local>
2023-09-12 17:40:11 +0300

    Added abstraction of Duck. Maybe remake it to the abstract class later

---
+++ src/abstractions/ducks/Duck.java
@@ -0,0 +1,23 @@
+package abstractions.ducks;
+
+public class Duck {
+
+    public Duck () {
+    }
+
+    public static void fly() {
+
+    }
+
+    public static void quack() {
+
+    }
+
+    public static void swim () {
+
+    }
+
+    public void display() {
+        // Не хватает данных для реализации метода. Какие должны быть поля и зачем он вообще нужен?
+    }
+}
